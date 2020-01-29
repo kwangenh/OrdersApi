618d49c8d7bd3951ef06573bf0e89e2b268a044d
Kev Mo <wangenheimk@gmail.com>
2020-01-28 19:55:19 -0700

    Add project files.

---
+++ OrdersApi/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrdersApi.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public Customer Customer { get; set; }
+        public List<ShipTo> ShipTos { get; set; }
+        public List<OrderItem> Items { get; set; }
+    }
+}
